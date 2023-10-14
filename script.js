fe658ed5b322b70fc193ab15c73fc6c770f585b1
Amma Obeid <amfo250@gmail.com>
2023-10-15 00:58:38 +0200

    final

--- script.js
+++ script.js
@@ -1,40 +1,79 @@
-// funktion för att spara användarnamn i localstorage
-function setLocalStorage() {
-    localStorage.setItem("username", inputUserName.value);
-    localStorage.setItem("password", inputPassword.value);
+const inputName = "Bella";
+const inputPass = "qwe123";
+
+let inputUserName = document.getElementById("userName");
+console.log("userName")
+let inputPassword = document.getElementById("passWord");
+console.log("passWord");
+
+
+function checkLogin(){
+  inputUserName = document.getElementById("userName").value;
+  inputPassword = document.getElementById("passWord").value;
+
+  if(inputUserName === inputName && inputPassword === inputPass){
+    localStorage.setItem("user", inputUserName);
+    localStorage.setItem("code", inputPassword);
+    welcomePage();
+}
+else{
+    backPage();
+}
 }
 
-var namn = 'Bella';
-var pass = 'qwe123';
-
-if (
-    localStorage.getItem("username") === namn &&
-    localStorage.getItem("password") === lösenord
-  ) {
-    login();
-  }
-
-const logInBtn = document.getElementById("logInBtn");
+document.getElementById("logInButton").addEventListener("click", function(){
+    checkLogin();
+});
 
 
+function welcomePage(){
 
+    document.body.innerHTML = "";
+    document.body.insertAdjacentHTML("afterbegin", '<h1>Welcome Bella</h1></br></br>');
 
-  logInBtn.addEventListener("click", function () {
-    if (inputUserName.value === namn && inputPassword.value === lösenord) {
-      console.log('hi');
-    }
-      
-  });
+    var braInlog = document.createElement('div');
+    braInlog.className='welcome';
+    braInlog.id= 'bra-info';
+    braInlog.setAttribute('title', ' Hello');
+    var braInlogText= document.createTextNode('Welcome here Bella');
+    braInlog.appendChild(braInlogText);
+    document.body.appendChild(braInlog);
 
+    const logOutButton = document.createElement('button');
+    logOutButton.innerHTML = "Log Out";
+    logOutButton.id = "loggaUt";
+    document.body.appendChild(logOutButton);
+    logOutButton.addEventListener("click", function(){
+     localStorage.clear();
+     gohome();
+    });
+}
+function backPage() {
+    document.body.innerHTML = "";
+    document.body.insertAdjacentHTML("afterbegin", '<h1>Sorry you did something wrong</h1></br>');
 
-/*function loginPage(form) {
-    if (form.userName.value === user) { 
-    if (form.passWord.value === pass) {              
-    location="home.html" 
-    } else {
-    alert("Incorrect detail Password")
-    }
-    } else {  alert("Incorrect detail Username")
-    }
-    }
-    */
+    var felInlog = document.createElement('div');
+    felInlog.className='Något-Fel';
+    felInlog.id= 'fel-info';
+    felInlog.setAttribute('title', ' Go back');
+    var felInlogText= document.createTextNode('User Name or PassWord are Wrong');
+    felInlog.appendChild(felInlogText);
+    document.body.appendChild(felInlog);
+    
+    const goBackButton = document.createElement('button');
+    goBackButton.innerHTML = "Go Back";
+    goBackButton.id = "newBtn";
+    document.body.appendChild(goBackButton);
+    goBackButton.addEventListener("click", function(){
+        gohome();
+    });
+}
+
+function gohome()
+{
+window.location.href="index.html";
+}
+
+if (localStorage.getItem("user", "code")){
+    welcomePage();
+}
